fb77484bddbe89faf9cf172bb1a7dcc37be259a9
chieccocluaharrypotter <chieccocluaharrypotter@gmail.com>
2026-02-14 20:09:05 +0000

    adding post function

--- model.py
+++ model.py
@@ -1,6 +1,27 @@
 import sqlalchemy 
 from pydantic import BaseModel 
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+
 
 app = FastAPI()
 
+@app.get("/")
+def root ():
+    return {"Hello":"World"}
+
+
+@app.get("/about")
+def detail ():
+    return {"This is about me"}
+
+
+@app.get("/report")
+def detail ():
+    return {"This is about me"}
+
+
+@app.post("/")
+def addItem(task:str):
+    newId = len(fakeDatabase.keys()) + 1
+    fakeDatabase[newId] = {"task":task}
+    return {"Data post successfully"}
