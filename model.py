7b658cfa2ebfe3cb9daf9bbf8ac736a4012509e0
chieccocluaharrypotter <chieccocluaharrypotter@gmail.com>
2026-02-15 11:17:47 +0000

    add default GET operation

--- model.py
+++ model.py
@@ -5,6 +5,15 @@ from fastapi import FastAPI, Body
 
 app = FastAPI()
 
+
+@app.api_route("/{full_path:path}", methods=["GET"])
+    # Log or handle the request as a default
+    print(f"Default GET handler called for path: {full_path}")
+    
+    # You can return a default response or an error
+    return {"message": f"Default GET operation for path: /{full_path}. No specific handler found."}
+
+
 @app.get("/")
 def root ():
     return {"Hello":"World"}
