82d3d4dcb231ff3288b27ca026eded7d6e1a8f69
chieccocluaharrypotter <chieccocluaharrypotter@gmail.com>
2026-02-18 15:35:51 +0000

    add table

--- main.py
+++ main.py
@@ -8,3 +8,33 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 print(f"Connecting to database at: {DATABASE_URL}")
+
+@app.api_route("/{full_path:path}", methods=["GET"])
+    # Log or handle the request as a default
+        print(f"Default GET handler called for path: {full_path}")
+            
+                # You can return a default response or an error
+                    return {"message": f"Default GET operation for path: /{full_path}. No specific handler found."}
+
+
+                    @app.get("/")
+                    def root ():
+                        return {"Hello":"World"}
+
+
+                        @app.get("/about")
+                        def detail ():
+                            return {"This is about me"}
+
+
+                            @app.get("/report")
+                            def detail ():
+                                return {"This is about me"}
+
+
+                                @app.post("/")
+                                def addItem(task:str):
+                                    newId = len(fakeDatabase.keys()) + 1
+                                        fakeDatabase[newId] = {"task":task}
+                                            return {"Data post successfully"}
+                                            
--- model.py
+++ model.py
@@ -8,31 +8,8 @@ from database import Base
 app = FastAPI()
 
 
-@app.api_route("/{full_path:path}", methods=["GET"])
-    # Log or handle the request as a default
-    print(f"Default GET handler called for path: {full_path}")
-    
-    # You can return a default response or an error
-    return {"message": f"Default GET operation for path: /{full_path}. No specific handler found."}
+class productType (Base):
+    __tablename__ = "productType"
 
-
-@app.get("/")
-def root ():
-    return {"Hello":"World"}
-
-
-@app.get("/about")
-def detail ():
-    return {"This is about me"}
-
-
-@app.get("/report")
-def detail ():
-    return {"This is about me"}
-
-
-@app.post("/")
-def addItem(task:str):
-    newId = len(fakeDatabase.keys()) + 1
-    fakeDatabase[newId] = {"task":task}
-    return {"Data post successfully"}
+    id= Column(Integer, primary_key=True, , index=True)
+    product_type_name= Column(String, index=True)
